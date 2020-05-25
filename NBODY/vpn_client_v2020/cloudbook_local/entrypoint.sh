85b2a273dc339f61eda4e1c07359d2feab0344f9
franabexperis <francisco.abril@experis.es>
2020-05-25 18:35:33 +0200

    Refresh Script to force NFS to update dir

--- NBODY/vpn_client_v2020/cloudbook_local/entrypoint.sh
+++ NBODY/vpn_client_v2020/cloudbook_local/entrypoint.sh
@@ -2,13 +2,14 @@
 
 openvpn --config /etc/openvpn/client/client01.ovpn &
 
-sleep 10s
+sleep 10
 echo "Start Mount DIRs"
 mount -o nfsvers=3,nolock 10.10.0.1:/share /share -t nfs
 mkdir -p /root/cloudbook/base_project/distributed
 mount -v -t nfs -o nfsvers=3,nolock 10.10.0.1:/root/cloudbook/base_project/distributed /root/cloudbook/base_project/distributed
 mkdir /root/cloudbook/base_project/agents
 #python3 /etc/cloudbook/cloudbook_agent/agent.py create -agent_id "" -project_folder base_project
+sh /root/refresh.sh &
 
 while :
 do
---
+++ NBODY/vpn_client_v2020/cloudbook_local/refresh.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+while true; do
+  ls /root/cloudbook/base_project/distributed > /dev/null
+  sleep 2
+done
